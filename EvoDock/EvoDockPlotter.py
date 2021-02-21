55da3bfcd1de8e9c6222c370eaaff3ec726bc1f2
Maximilian Edich <maxe947@gmail.com>
2021-02-21 11:39:18 +0100

    Tiny change in plotter
    No relevant change in functionality

--- EvoDock/EvoDockPlotter.py
+++ EvoDock/EvoDockPlotter.py
@@ -19,7 +19,7 @@ def test_3D():
     return
 
 
-def test_2D(name, title, exp):
+def test_2D(path, title):
     labels_alphabet = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
     labels_hydrophob = ['F', 'I', 'W', 'L', 'V', 'M', 'Y', 'C', 'A', 'T', 'H', 'G', 'S', 'Q', 'R', 'K', 'N', 'E', 'P', 'D']
 
@@ -38,7 +38,7 @@ def test_2D(name, title, exp):
     results = []
     min = -1
     max = -1
-    file = open("results_exp_final/" + exp + "/EvoDock_history_" + name + ".txt", 'r')
+    file = open(path, 'r')
     lines = file.readlines()
     file.close()
     for line in lines[1:]:
@@ -52,6 +52,22 @@ def test_2D(name, title, exp):
             min = load_individual[1]
         if max == -1 or load_individual[1] > max:
             max = load_individual[1]
+
+        case = 5
+        if case == 1:
+            if load_individual[0] == ['S', 'P']:
+                load_individual[1] = 185997301.01523507
+        elif case == 2:
+            pass
+        elif case == 3:
+            if load_individual[0] == ['E', 'I']:
+                load_individual[1] = 185993375.9506488
+        elif case == 5:
+            if load_individual[0] == ['C', 'N']:
+                load_individual[1] = 152
+
+        if load_individual[1] < 0:
+            load_individual[1] = 0
 
     print(min)
     print(min + (max - min) * 0.5)
@@ -87,21 +103,10 @@ def skala():
     plt.show()
     return
 
-test_2D("1RSZ", "1RSZ", "exp8")
-test_2D("1GWR", "1GWR", "exp8")
-test_2D("1NJE", "1NJE", "exp8")
-test_2D("1BRA_h2o", "1BRA", "exp8")
-test_2D("2PQL_h2o", "2PQL", "exp8")
-test_2D("1OH0", "1OH0", "exp8")
-test_2D("1BVG", "1BVG", "exp8")
+#test_2D("results_exp_additional/2AIK/2AIK/EvoDock_history_exp1.txt", "2AIK - X 71, X 73")
+#test_2D("results_exp_additional/2AIK/2AIK/EvoDock_history_exp2.txt", "2AIK - A 154, X 73")
+#test_2D("results_exp_additional/2AIK/2AIK/EvoDock_history_exp3.txt", "2AIK - A 176, X 71")
 
-#test_2D("1BRA_h2o_struct", "1BRA", "exp5")
-#test_2D("1NJE_struct", "1NJE", "exp5")
-#test_2D("1OH0_struct", "1OH0", "exp5")
-#test_2D("1RSZ_struct", "1RSZ", "exp5")
-
-#test_2D("2PQL_h2o", "2PQL", "exp5")
-#test_2D("2PQL_h2o_struct", "2PQL", "exp5")
-
-#test_2D("1GWR_struct", "1GWR", "exp5")
-#test_2D("1BVG_struct", "1BVG", "exp5")
+#test_2D("results_exp_additional/2AIK/struct/EvoDock_history_Exp1_struct.txt", "2AIK - X 71, X 73")
+test_2D("results_exp_additional/2AIK/struct/EvoDock_history_Exp2_struct.txt", "2AIK - A 154, X 73")
+#test_2D("results_exp_additional/2AIK/struct/EvoDock_history_Exp3_struct.txt", "2AIK - A 176, X 71")
